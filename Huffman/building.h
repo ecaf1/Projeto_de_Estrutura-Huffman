382da4c3ce81f07f970852e7627b503c447a3cbf
Donvicton <117011568+Donvicton@users.noreply.github.com>
2023-05-09 16:00:01 -0300

    Add files via upload

---
+++ Huffman/building.h
@@ -0,0 +1,8 @@
+#include "tree.h"
+#include "queue.h"
+
+Tree *func_exception(FILE *arquivo);
+Tree* rebuild_huffman_tree (FILE *arquivo);
+Tree* build_huffman_tree(int *array);
+Tree *build_Tree(priority_queue *pq);
+int creating_huffman_string(Tree *huffman, FILE *header);
